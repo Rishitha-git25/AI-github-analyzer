24793020192de7bffe8864129a7a5ec955524cf5
Rishitha git25 <rishitha2181@gmail.com>
2026-03-21 19:26:42 +0530

    fixed render port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const repoRoutes = require('./routes/repoRoutes');
 const app = express();
-const PORT =  5000;
+const PORT =  process.env.PORT || 5000;
 
 app.use(cors());//allow frontend requests from different origins (like localhost:3000 for React) to access the backend API without CORS issues
 app.use(express.json());// Middleware to parse JSON bodies from incoming requests
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Backend is running on http://localhost:${PORT}`);
+    console.log(`Backend is running on ${PORT}`);
 });
